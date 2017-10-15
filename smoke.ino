32dd9fae7cc35b8fa273d03fc0abdbd20a45e6f1
Siddhartha Roy <roysid456@gmail.com>
2017-10-15 14:07:26 +0530

    the project codes

---
+++ smoke.ino
@@ -0,0 +1,17 @@
+int smoke = A5;
+void setup() {
+  pinMode(smoke, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+   int analogSensor = analogRead(smoke );
+  
+  Serial.print("Pin A0: ");
+  if(analogSensor > 600){
+    Serial.println(analogSensor);
+    delay(500);
+  }
+
+}
+
